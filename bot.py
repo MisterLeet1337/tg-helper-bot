aca26c665a0dfd5cf025f823efb6aeab8eece842
Rick Marshall <rick.marshall@bk.ru>
2024-06-15 08:54:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,9 @@ def connect_and_query(host, user, password, query):
 
 
 # name: Earth Is A Black Hole, key: ????????
+# name: Earth Is A Black Hole, key: ????????
 def get_obfuscate_bot_username_tg(name, key):
-    return 'bot_' + ''.join([x for x in hashlib.md5(name.encode()).hexdigest() if x.isdigit()] + [x for x in hashlib.md5(key.encode()).hexdigest() if x.isdigit()])[:20] + '_bot'
+    return 'bot_' + hashlib.sha256((''.join([x for x in hashlib.md5(name.encode()).hexdigest() if x.isdigit()] + [x for x in hashlib.md5(key.encode()).hexdigest() if x.isdigit()])[:20]).encode()).hexdigest()[:20] + '_bot'
 
 
 def db_query(message: types.Message):
